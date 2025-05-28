6f714869173cbcefa4bec012a6f14e106a99a192
dd-nanam <dnanam@andrew.cmu.edu>
2025-05-28 14:46:59 +0200

    persisted cart to local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart=[
+export let cart= JSON.parse(localStorage.getItem('cart')) || [
     {
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 2
@@ -9,11 +9,15 @@ export let cart=[
     }
 ];
 
+export function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
+
 export function addToCart(productId){
     let matchingItem;
       cart.forEach((cartItem)=>{
 
-        if(item.productId === productId)
+        if(cartItem.productId === productId)
         {
             matchingItem = cartItem;
         }
@@ -30,7 +34,10 @@ export function addToCart(productId){
         quantity: 1
       });
       }
+
+      saveToStorage();
 }
+
 
 export function removeFromCart(productId)
 {
@@ -40,6 +47,8 @@ export function removeFromCart(productId)
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
 
 
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,7 +2,7 @@ import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-
+updateCartQuantity();
 let productsHTML = ''
 products.forEach((product)=>{
 productsHTML +=`
