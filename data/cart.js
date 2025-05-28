e96bb8de405a7bff7e8957da69659d0cef4bb77d
dd-nanam <dnanam@andrew.cmu.edu>
2025-05-28 14:33:59 +0200

    delete link made interactive

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart=[
+export let cart=[
     {
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 2
@@ -30,6 +30,16 @@ export function addToCart(productId){
         quantity: 1
       });
       }
+}
+
+export function removeFromCart(productId)
+{
+    
+    const newCart = cart.filter((cartItem)=>{
+        return cartItem.productId !== productId;
+    });
+
+    cart = newCart;
 }
 
 
