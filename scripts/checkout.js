af450b774f82467bf5820df544fa802c395f0120
dd-nanam <dnanam@andrew.cmu.edu>
2025-05-28 14:38:21 +0200

    remove html container on delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -19,7 +19,7 @@ cart.forEach((cartItem)=>{
    
         cartHTML += `
     
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingItem.id}">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -104,5 +104,9 @@ document.querySelectorAll('.js-delete-link')
             let productID = link.dataset.productId;
             removeFromCart(productID);
             console.log(cart);
+            const container = document.querySelector(
+                `.js-cart-item-container-${productID}`
+            );
+            container.remove();
         });
     });
