842f85704dd2313f7438dbbba9c67a4bb0fd7b22
dd-nanam <dnanam@andrew.cmu.edu>
2025-06-09 10:51:36 +0200

    set up tests for money with jasmine

---
+++ tests-jasmine/MoneyTests.js
@@ -0,0 +1,15 @@
+import { formatCurrency } from '../scripts/utils/money.js';
+
+describe('Test suite: format current',() => {
+    it('converts cents into dollars',() => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('works with zero',()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+
+    it('Rounding up',() =>{
+        expect(formatCurrency(2000.5)).toEqual('20.01')
+    })
+});
