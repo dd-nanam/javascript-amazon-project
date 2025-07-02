e986765ac5b89861d36b667a9cc47f4c3082bcf4
dd-nanam <dnanam@andrew.cmu.edu>
2025-07-02 09:26:29 +0200

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,25 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 //import '../data/backend-practice.js';
+
+async function loadPage(){
+    
+    await loadProductsFetch();
+
+    await new Promise((resolve)=>{
+            loadCart(()=>{
+                resolve(); 
+            })
+        })
+
+    renderOrderSummary();
+    renderPaymentSummary();
+
+    
+}
+loadPage();
+
+/*
 Promise.all([
     loadProductsFetch(),
     new Promise((resolve)=>{
@@ -15,7 +34,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 })
-
+*/
 /*
 new Promise((resolve) =>{
     loadProducts(()=>{
